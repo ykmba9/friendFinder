f8f18b810788a98cdbe91b01c1107563b757643b
Yamini Yadav <ykmba9@gmail.com>
2018-08-13 12:50:47 -0700

    files added

---
+++ app/data/friends.js
@@ -0,0 +1,21 @@
+
+var friendList = [
+    {
+      name: 'abc',
+      profilePic: 'https://google.com/butterfly',
+      scores: [
+        "4",
+        "5",
+        "5",
+        "2",
+        "4",
+        "5",
+        "4",
+        "4",
+        "3",
+        "4"
+      ]
+    }
+  ]
+  
+  module.exports = friendList;
